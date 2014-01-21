83dda00263fe1369b9ef3d7b2e41d384a729d3fe
elviosc <elviosc@hotmail.com>
2014-01-21 12:18:21 -0200

    Primeiro código

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+header('Location: http://elvio.com.br')
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title></title>
+    </head>
+    <body>
+        
+    </body>
+</html>
