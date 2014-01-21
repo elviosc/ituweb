f39c1d5a5c89d1cac808e18af51b289f0753d53e
elviosc <elviosc@hotmail.com>
2014-01-21 12:45:58 -0200

    novo site

---
+++ Includes/simplecms-config.php
@@ -0,0 +1,9 @@
+<?php
+    define('DB_NAME', 'simplecms253');
+    define('DB_USER', 'simplecmsuser253');
+    define('DB_PASSWORD', 'r|*#]^IopIeL');
+    define('DB_HOST', 'localhost');
+
+    define('DEFAULT_ADMIN_USERNAME', 'admin');
+    define('DEFAULT_ADMIN_PASSWORD', '123');
+?>
